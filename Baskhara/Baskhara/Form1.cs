0a980ea4b7f6f7477273f9c9e00de56f01627987
marcio777 <marcio.infra@hotmail.com>
2018-03-15 23:34:40 -0300

    ALURA
    Curso ALURA

---
+++ Baskhara/Baskhara/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baskhara
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 2;
+            int b = 5;
+            int c = 2;
+
+            double delta;
+            double a1;
+            double a2;
+
+            delta = b * b - a * a * c;
+            a1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            a2 = (-b + Math.Sqrt(delta)) / (2 * a);
+
+            MessageBox.Show("Resultado 1 : " + a1);
+            MessageBox.Show("Resultado 2 : " + a2);
+        }
+    }
+}
